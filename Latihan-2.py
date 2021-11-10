19531cfe07266aa9136a388ace07385d329ac393
silviadelya <silviadelyaheryani31@gmail.com>
2021-11-10 13:26:49 +0700

    program

---
+++ Latihan-2.py
@@ -0,0 +1,10 @@
+print("Latihan 2")
+print()
+a = 0
+while True:
+        angka = int(input("Masukkan Angka:"))
+        if a < angka:
+                a = angka
+        if angka == 0:
+                break
+print("Angka Terbesar:", a)
